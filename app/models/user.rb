7faa0501c821ce9833d8103ba4c81e81a6e91d80
adillera <aj.dillera@gmail.com>
2025-11-27 06:13:24 +0800

    Added initial check if logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum :status, { active: "active", inactive: "inactive" }
 end
